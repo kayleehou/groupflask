bc7b96e1ac8a413cc38d1c89573fbb51d6c0dbda
kayleehou <kayleehou@yahoo.com>
2022-11-03 00:58:39 -0700

    change

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from api import app_api # Blueprint import api definition
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
+from model_mcu import MCU_api
 
-app.register_blueprint(app_api) # register api routes
+app.register_blueprint(MCU_api)
 app.register_blueprint(app_projects) # register api routes
 
 @app.errorhandler(404)  # catch for URL not found
--- model_mcu.py
+++ model_mcu.py
@@ -2,58 +2,65 @@ from contextlib import nullcontext
 from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
 from flask_restful import Api, Resource # used for REST API building
 import requests  # used for testing 
-# import time
+import time
 
 # Blueprints enable python code to be organized in multiple files and directories https://flask.palletsprojects.com/en/2.2.x/blueprints/
-mcu_api = Blueprint('mcu_api', __name__,
-                   url_prefix='/api/mcu')
+MCU_api = Blueprint('MCU_api', __name__,
+                   url_prefix='/api/MCU')
 
 # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
-api = Api(mcu_api)
+api = Api(MCU_api)
 
 """Time Keeper
 Returns:
     Boolean: is it time to update?
 """
-# def updateTime():
-#     global last_run  # the last_run global is preserved between calls to function
-#     try: last_run
-#     except: last_run = None
+def updateTime():
+    global last_run  # the last_run global is preserved between calls to function
+    try: last_run
+    except: last_run = None
     
-#     # initialize last_run data
-#     if last_run is None:
-#         last_run = time.time()
-#         return True
+    # initialize last_run data
+    if last_run is None:
+        last_run = time.time()
+        return True
     
-#     # calculate time since last update
-#     elapsed = time.time() - last_run
-#     if elapsed > 86400:  # update every 24 hours 60*60*24
-#         last_run = time.time()
-#         return True
+    # calculate time since last update
+    elapsed = time.time() - last_run
+    if elapsed > 86400:  # update every 24 hours
+        last_run = time.time()
+        return True
     
-#     return False
+    return False
 
 """API Handler
 Returns:
     String: API response
 """   
 def getMCUAPI():
-    global mcu_data  # the covid_data global is preserved between calls to function
-    try: mcu_data
-    except: mcu_data = None
+    global MCU_data  # the covid_data global is preserved between calls to function
+    try: MCU_data
+    except: MCU_data = None
 
     """
     Preserve Service usage / speed time with a Reasonable refresh delay
     """
-  # request Covid data
+    if updateTime(): 
+        """
+        RapidAPI is the world's largest API Marketplace. 
+        Developers use Rapid API to discover and connect to thousands of APIs. 
+        """
+        url = "https://mcu-comics-and-characters.p.rapidapi.com/mcu/comics"
+        headers = {
+            "X-RapidAPI-Key": "40507ace9fmshcdcac9ccdd404c3p1d6853jsn21cc92ac2d5e",
+	        "X-RapidAPI-Host": "mcu-comics-and-characters.p.rapidapi.com"
+        }
+        response = requests.request("GET", url, headers=headers)
+        covid_data = response
+    else:  
+        response = MCU_data
 
-url = "https://mcu-comics-and-characters.p.rapidapi.com/mcu/comics"
-headers = {
-    "X-RapidAPI-Key": "40507ace9fmshcdcac9ccdd404c3p1d6853jsn21cc92ac2d5e",
-    "X-RapidAPI-Host": "mcu-comics-and-characters.p.rapidapi.com"
-}
-response = requests.request("GET", url, headers=headers)
-mcu_data = response
+    return response
 
 
 """API with Country Filter
@@ -61,28 +68,30 @@ Returns:
     String: Filter of API response
 """   
 def getMCU(filter):
-    # Request MCU Data
+    # Request Covid Data
     response = getMCUAPI()
-    # Look for Comic   
+    # Look for Country    
+
     comics.pop(0)
-    comics = response.json().get('MCU List')
+  
+    comics = response.json().get('comic name')
     for comic in comics:  # countries is a list
         if comic["title"].lower() == filter.lower():  # this filters for country
-            return comic["link"]
+            return comic
     
-    return {"error": filter + " not in our database... sorry :(. Try searching something else!"}
+    return {"message": filter + " not found"}
 
 
 """Defines API Resources 
   URLs are defined with api.add_resource
 """   
 class MCUAPI:
-    """API Method to GET all MCU Data"""
+    """API Method to GET all Covid Data"""
     class _Read(Resource):
         def get(self):
             return getMCUAPI().json()
         
-    """API Method to GET Covid Data for a Specific Comic"""
+    """API Method to GET Covid Data for a Specific Country"""
     class _ReadMCU(Resource):
         def get(self, filter):
             return jsonify(getMCU(filter))
@@ -105,21 +114,19 @@ if __name__ == "__main__":
 
     # This code looks for "world data"
     response = getMCUAPI()
-    print("MCU List:")
     comics = response.json()
     comics.pop(0)
+  
     for comic in comics:
         print(comic["title"]) 
         print("\t" + comic["link"])
 
     print("-"*30)
 
-    # This code looks for USA in "MCU List"
-    comic = getMCU("Avengers: Endgame Prelude")
-    print("Avengers: Endgame Prelude Link")
-    for key, value in comic.items():
-        print(key, value)
+    # This code looks for USA in "countries_stats"
+    comic = getMCU("Ant-Man Prelude")
+    print("Ant-Man Link")
+    for comic["title"], comic["link"] in comic.items():
+        print(comic["title"], comic["link"])
         
     print("-"*30)
-
-
